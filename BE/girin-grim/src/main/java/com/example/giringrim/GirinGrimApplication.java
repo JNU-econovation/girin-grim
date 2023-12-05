408e1327ee46f5c483b4bfeb958bc5de32b94080
MinjuKwak <mkwak1125@gmail.com>
2023-12-06 00:03:17 +0900

    docs : 초기 개발 세팅

---
+++ BE/girin-grim/src/main/java/com/example/giringrim/GirinGrimApplication.java
@@ -0,0 +1,13 @@
+package com.example.giringrim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GirinGrimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GirinGrimApplication.class, args);
+	}
+
+}
---
+++ BE/girin-grim/src/test/java/com/example/giringrim/GirinGrimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.giringrim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GirinGrimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
