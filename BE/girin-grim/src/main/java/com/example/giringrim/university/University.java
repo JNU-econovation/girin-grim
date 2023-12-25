5018dc187ea5cc26bfd607e5c3f22969beba0746
minju <mkwak1125@gmail.com>
2023-12-25 22:00:33 +0900

    feat : University entity 작성

---
+++ BE/girin-grim/src/main/java/com/example/giringrim/university/University.java
@@ -0,0 +1,32 @@
+package com.example.giringrim.university;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "UNIVERSITY")
+public class University {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String region;
+
+    @Builder
+    public University(String name, String region){
+        this.name = name;
+        this.region = region;
+    }
+}
+
