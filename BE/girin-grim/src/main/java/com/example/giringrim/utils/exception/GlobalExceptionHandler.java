a54965c3d23b353e0938967bc3c7f56e53d3063b
minju <mkwak1125@gmail.com>
2023-12-25 22:05:46 +0900

    feat : university관련 exception handler 생성

--- BE/girin-grim/src/main/java/com/example/giringrim/utils/exception/GlobalExceptionHandler.java
+++ BE/girin-grim/src/main/java/com/example/giringrim/utils/exception/GlobalExceptionHandler.java
@@ -3,6 +3,8 @@ package com.example.giringrim.utils.exception;
 import com.example.giringrim.member.exception.EmailAlreadyExistException;
 import com.example.giringrim.member.exception.NicknameAlreadyExistException;
 import com.example.giringrim.member.exception.UniversitySelectionException;
+import com.example.giringrim.university.exception.RegionNotExistException;
+import com.example.giringrim.university.exception.UnivNotExistException;
 import com.example.giringrim.utils.ApiResponse;
 import com.example.giringrim.utils.ApiResponseGenerator;
 import org.springframework.http.HttpStatus;
@@ -25,6 +27,16 @@ public class GlobalExceptionHandler{
     @ExceptionHandler(NicknameAlreadyExistException.class)
     public ApiResponse<ApiResponse.CustomBody> handleNicknameAlreadyExistException(final Exception e){
         return ApiResponseGenerator.fail(ErrorMessage.NICKNAME_ALREADY_EXIST.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(UnivNotExistException.class)
+    public ApiResponse<ApiResponse.CustomBody> handleUnivNotExistException(final Exception e){
+        return ApiResponseGenerator.fail(ErrorMessage.UNIV_NOT_EXIST.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(RegionNotExistException.class)
+    public ApiResponse<ApiResponse.CustomBody> handleRegionNotExistException(final Exception e){
+        return ApiResponseGenerator.fail(ErrorMessage.REGION_NOT_EXIST.getMessage(), HttpStatus.NOT_FOUND);
     }
 
 
