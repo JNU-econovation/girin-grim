93977cefc78eb1fa6b5b1bbe714de09fa7782b34
minju <mkwak1125@gmail.com>
2023-12-25 16:46:21 +0900

    feat : controller에 중복체크 메서드 생성

--- BE/girin-grim/src/main/java/com/example/giringrim/member/MemberController.java
+++ BE/girin-grim/src/main/java/com/example/giringrim/member/MemberController.java
@@ -23,7 +23,23 @@ public class MemberController {
         return ApiResponseGenerator.success(HttpStatus.OK);
     }
 
+    /*
+     * 회원가입할때 이메일 중복체크
+     */
+    @PostMapping("/member/login/email")
+    public ResponseEntity<?> emailValidation(@RequestBody MemberReqDto.EmailValidationReqDto reqDto){
+        memberService.emailValidation(reqDto.getEmail());
+        return ApiResponseGenerator.success(HttpStatus.OK);
+    }
 
+    /*
+     * 회원가입할때 닉네임 중복체크
+     */
+    @GetMapping("/member/login/nickname")
+    public ResponseEntity<?> nicknameValidation(@RequestBody MemberReqDto.NicknameValidationReqDto reqDto){
+        memberService.nicknameValidation(reqDto.getNickname());
+        return ApiResponseGenerator.success(HttpStatus.OK);
+    }
 
 
 }
