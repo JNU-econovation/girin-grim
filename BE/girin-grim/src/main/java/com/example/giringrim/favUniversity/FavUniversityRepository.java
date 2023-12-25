8c86808bcd62ab8d11855bd2b6d762d5f0b8846c
minju <mkwak1125@gmail.com>
2023-12-25 16:04:45 +0900

    feat : FavUniversityRepository 생성

---
+++ BE/girin-grim/src/main/java/com/example/giringrim/favUniversity/FavUniversityRepository.java
@@ -0,0 +1,6 @@
+package com.example.giringrim.favUniversity;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FavUniversityRepository extends JpaRepository<FavUniversity, Long> {
+}
