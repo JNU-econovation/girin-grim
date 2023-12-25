6ec700df82e333fddf0247bc72fa8b30f154811c
minju <mkwak1125@gmail.com>
2023-12-25 21:43:36 +0900

    feat : Member Service 작성

---
+++ BE/girin-grim/src/main/java/com/example/giringrim/member/MemberServiceImpl.java
@@ -0,0 +1,59 @@
+package com.example.giringrim.member;
+
+import com.example.giringrim.member.exception.EmailAlreadyExistException;
+import com.example.giringrim.favUniversity.FavUniversityRepository;
+import com.example.giringrim.member.exception.NicknameAlreadyExistException;
+import com.example.giringrim.member.exception.UniversitySelectionException;
+import com.example.giringrim.utils.exception.ErrorMessage;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class MemberServiceImpl implements MemberService {
+
+    private final MemberRepository memberRepository;
+    private final FavUniversityRepository favUniversityRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public void join(MemberReqDto.JoinReqDto joinReqDto) {
+        String encodedPassword = passwordEncoder.encode(joinReqDto.getPassword());
+        Member member = joinReqDto.toEntity(encodedPassword);
+        memberRepository.save(member);
+
+        if(joinReqDto.getUniversity().isEmpty() || joinReqDto.getUniversity().size() >=10){
+            //TODO 대학교 선택을 하지 않았을 때, 10개 이상 선택했을때 예외처리
+            throw new UniversitySelectionException(ErrorMessage.SELECTED_WRONG_UNIVERSITY);
+        }
+        joinReqDto.getUniversity().forEach(university -> {
+            favUniversityRepository.save(university.toEntity(member));
+        });
+
+    }
+
+    @Override
+    public void emailValidation(String email) {
+        Member member = memberRepository.findByEmail(email);
+        if(member != null){
+            throw new EmailAlreadyExistException(ErrorMessage.EMAIL_ALREADY_EXIST);
+        }
+    }
+
+    @Override
+    public void nicknameValidation(String nickname) {
+        Member member = memberRepository.findByNickname(nickname);
+        if(member != null){
+            throw new NicknameAlreadyExistException(ErrorMessage.NICKNAME_ALREADY_EXIST);
+        }
+    }
+
+    @Override
+    public void login(MemberReqDto.LoginReqDto loginReqDto) {
+
+    }
+
+}
