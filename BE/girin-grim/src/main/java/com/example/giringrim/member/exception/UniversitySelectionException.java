fe708325f0d0b2354a1698b404f1e2d79e00993b
minju <mkwak1125@gmail.com>
2023-12-25 22:10:09 +0900

    feat : 관심대학 설정 exception class 생성

---
+++ BE/girin-grim/src/main/java/com/example/giringrim/member/exception/UniversitySelectionException.java
@@ -0,0 +1,13 @@
+package com.example.giringrim.member.exception;
+
+import com.example.giringrim.utils.exception.ErrorMessage;
+import lombok.Getter;
+
+@Getter
+public class UniversitySelectionException extends RuntimeException{
+    private final ErrorMessage errorMessage;
+
+    public UniversitySelectionException(ErrorMessage errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+}
