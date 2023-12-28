328a413f6072787f664c6c72e0fa1fa83b9c4e8f
minju <mkwak1125@gmail.com>
2023-12-29 05:46:02 +0900

    feat : refresh token을 생성해주는 함수 추가

--- BE/girin-grim/src/main/java/com/example/giringrim/utils/security/TokenGenerator.java
+++ BE/girin-grim/src/main/java/com/example/giringrim/utils/security/TokenGenerator.java
@@ -39,6 +39,16 @@ public class TokenGenerator {
                 .compact();
     }
 
+    //Refresh 토큰 생성하는 함수
+    public String createRefreshToken(Member member) {
+        return Jwts.builder().setSubject(member.getEmail()) // 정보 저장
+                .setHeaderParam("typ", "JWT")
+                .setIssuedAt(new Date()) // 토큰 발행 시간
+                .setExpiration(calcExpirationDateTime(REFRESH_TOKEN)) // 토큰 만료 시간
+                .signWith(createKey(), SignatureAlgorithm.HS256)  // 암호화 알고리즘 및 secretKey
+                .compact();
+    }
+
     //두 종류의 토큰 만료 시간 설정
     private Date calcExpirationDateTime(final String type) {
         LocalDateTime currentTime = LocalDateTime.now();
