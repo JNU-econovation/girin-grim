d2cf337f36223d758ab6277565340ff5dda5b231
박건규 <geongyu09@gmail.com>
2023-12-29 20:31:16 +0900

    feat: join 페이지에서 로그인 되돌아가기 추가
    회원가입 버튼 밑에 로그인 페이지로 갈 수 있는 부분 추가
    
    #19

--- FE/src/components/join/SubmitBtn.tsx
+++ FE/src/components/join/SubmitBtn.tsx
@@ -2,5 +2,5 @@
 import StyledBtn from "../common/StyledBtn";
 
 export default function SubmitBtn() {
-  return <StyledBtn text="회원가입" />;
+  return <StyledBtn text="회원가입" style="mb-[1.8rem]" />;
 }
