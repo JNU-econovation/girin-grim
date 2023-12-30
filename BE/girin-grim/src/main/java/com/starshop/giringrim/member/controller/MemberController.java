cff67a2bc1ba4ee4e833f4de31caa0ec1a7c61e0
minju <mkwak1125@gmail.com>
2023-12-30 14:44:09 +0900

    feat : 헤더 로그인 정보 controller 추가

--- BE/girin-grim/src/main/java/com/starshop/giringrim/member/controller/MemberController.java
+++ BE/girin-grim/src/main/java/com/starshop/giringrim/member/controller/MemberController.java
@@ -4,10 +4,12 @@ import com.starshop.giringrim.member.dto.MemberReqDtos;
 import com.starshop.giringrim.member.dto.MemberRespDtos;
 import com.starshop.giringrim.member.service.MemberServiceImpl;
 import com.starshop.giringrim.utils.common.ApiResponseGenerator;
+import com.starshop.giringrim.utils.security.UserDetailsImpl;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -18,7 +20,7 @@ public class MemberController {
     private final MemberServiceImpl memberService;
 
     /*
-    * 회원가입 메서드
+    * 회원가입
      */
     @PostMapping("/member/join")
     public ResponseEntity<?> join(@Valid @RequestBody MemberReqDtos.JoinReqDto joinReqDto){
@@ -27,7 +29,7 @@ public class MemberController {
     }
 
     /*
-     * 회원가입할때 이메일, 닉네임 중복체크 메서드
+     * 회원가입할때 이메일, 닉네임 중복체크
      */
     @GetMapping("/member/join")
     public ResponseEntity<?> joinValidation(@RequestParam(value = "email", required = false) String email, @RequestParam(value = "nickname", required = false) String nickname){
@@ -36,7 +38,7 @@ public class MemberController {
     }
 
     /*
-     * 로그인 메서드
+     * 로그인
      */
     @PostMapping("/member/login")
     public ResponseEntity<?> login(@Valid @RequestBody MemberReqDtos.LoginReqDto loginReqDto){
@@ -45,12 +47,21 @@ public class MemberController {
     }
 
     /*
-     * 프로필 조회 메서드
+     * 프로필 조회
      */
     @GetMapping("/member/{memberId}")
-    public ResponseEntity<?> getProfile(@PathVariable("memberId") Long memberId){
-        MemberRespDtos.ProfileRespDto profileResDto = memberService.getProfile(memberId);
+    public ResponseEntity<?> getProfile(@PathVariable("memberId") Long memberId, @AuthenticationPrincipal UserDetailsImpl userDetails){
+        MemberRespDtos.ProfileRespDto profileResDto = memberService.getProfile(memberId, userDetails.getEmail());
         return ApiResponseGenerator.success(profileResDto, HttpStatus.OK);
+    }
+
+    /*
+     * 헤더 로그인 정보
+     */
+    @GetMapping("/member")
+    public ResponseEntity<?> getHeaderInfo(@AuthenticationPrincipal UserDetailsImpl userDetails){
+        MemberRespDtos.HeaderInfoRespDto headerInfoRespDto = memberService.getHeaderInfo(userDetails);
+        return ApiResponseGenerator.success(headerInfoRespDto, HttpStatus.OK);
     }
 
 }
