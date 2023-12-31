916ba6aebfd003019d58f25bca9d230b0a5c1d18
minju <mkwak1125@gmail.com>
2024-01-01 01:22:01 +0900

    feat : option entity 작성

---
+++ BE/girin-grim/src/main/java/com/starshop/giringrim/option/item/Item.java
@@ -0,0 +1,33 @@
+package com.starshop.giringrim.option.item;
+
+
+import com.starshop.giringrim.option.Option;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "ITEM")
+public class Item {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String itemName;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "option_id")
+    private Option option;
+
+
+    @Builder
+    public Item(String itemName, Option option){
+        this.itemName = itemName;
+        this.option = option;
+    }
+}
+
