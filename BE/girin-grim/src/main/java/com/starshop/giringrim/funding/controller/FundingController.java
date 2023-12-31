62e8dd2c9a86e9b84c560370866f80792de78834
minju <mkwak1125@gmail.com>
2024-01-01 00:32:39 +0900

    feat : 펀딩 작성 controller 추가

---
+++ BE/girin-grim/src/main/java/com/starshop/giringrim/funding/controller/FundingController.java
@@ -0,0 +1,33 @@
+package com.starshop.giringrim.funding.controller;
+
+import com.starshop.giringrim.funding.dto.FundingReqDtos;
+import com.starshop.giringrim.funding.dto.FundingRespDtos;
+import com.starshop.giringrim.funding.service.FundingServiceImpl;
+import com.starshop.giringrim.utils.common.ApiResponseGenerator;
+import com.starshop.giringrim.utils.security.UserDetailsImpl;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
+public class FundingController {
+
+    private final FundingServiceImpl fundingServiceImpl;
+
+    /*
+    *   펀딩 작성
+     */
+    @PostMapping("/funding")
+    public ResponseEntity<?> createFunding(@Valid @RequestBody FundingReqDtos.UploadDto uploadDto, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        fundingServiceImpl.createFunding(uploadDto, userDetails.getEmail());
+        return ApiResponseGenerator.success(HttpStatus.OK);
+
+    }
+
+    
+}
