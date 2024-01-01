ab344a956e50f83e2934f6afebdd76feedf1b1a5
minju <mkwak1125@gmail.com>
2024-01-01 15:56:24 +0900

    feat : item repository 작성

---
+++ BE/girin-grim/src/main/java/com/starshop/giringrim/option/item/ItemRepository.java
@@ -0,0 +1,11 @@
+package com.starshop.giringrim.option.item;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface ItemRepository extends JpaRepository<Item, Long> {
+    @Query("select i from Item i where i.option.id = :id")
+    List<Item> findAllByOptionId(Long id);
+}
