7ac22a085d85d9da3c2c84cb5fe7966ea28ade8c
박건규 <geongyu09@gmail.com>
2024-01-02 16:09:08 +0900

    deging: 홈 헤더 그리드 수정
    홈 헤더와 타입 글자 그리드 수정
    
    #25

--- FE/src/components/home/HomeHeader.tsx
+++ FE/src/components/home/HomeHeader.tsx
@@ -4,7 +4,7 @@ export default function HomeHeader() {
       <h1 className="text-color656 font-[700] font-notoKR text-[1.625rem] m-0 p-0">
         너와 그릴 기린 그림
       </h1>
-      <span className="text-colorb9b block mb-[0.2rem]">기부형 {">"}</span>
+      <span className="text-colorb9b block mb-[0.28rem]">기부형 {">"}</span>
     </div>
   );
 }
