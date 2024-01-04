ae8176ace0e957533cb75419c978f5727ef5ca01
minju <mkwak1125@gmail.com>
2024-01-04 20:16:35 +0900

    feat : 파라미터 값에 따른 동적 쿼리 작성

---
+++ BE/girin-grim/src/main/java/com/starshop/giringrim/funding/repository/FundingRepositoryCustom.java
@@ -0,0 +1,45 @@
+package com.starshop.giringrim.funding.repository;
+
+
+import com.querydsl.core.types.OrderSpecifier;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.core.types.dsl.NumberExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.starshop.giringrim.funding.dto.FundingRespDtos;
+import com.starshop.giringrim.funding.dto.QFundingRespDtos_HomeDto_FundingDto;
+import com.starshop.giringrim.funding.entity.FundingType;
+import com.starshop.giringrim.funding.entity.QFunding;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.starshop.giringrim.funding.entity.QFunding.funding;
+
+@Repository
+@RequiredArgsConstructor
+public class FundingRepositoryCustom {
+
+    private final JPAQueryFactory queryFactory;
+
+
+    public List<FundingRespDtos.HomeDto.FundingDto> search(FundingSearchCondition condition) {
+        return queryFactory
+                .select(new QFundingRespDtos_HomeDto_FundingDto(funding, funding.member))
+                .from(funding)
+                .where(
+                        funding.university.id.in(universityIdEq(condition.getUniversityId(), condition.getUniversityIds())),
+                        keywordEq(condition.getKeyword()),
+                        categoryEq(condition.getCategory())
+                )
+                .offset(condition.getPageable().getOffset())
+                .limit(condition.getPageable().getPageSize())
+                .orderBy(
+                        sortEq(condition.getSort())
+                )
+                .fetch();
+
+    }
+}
