a947c6198de96072334cf7ef04f3cd71b31fa331
박건규 <geongyu09@gmail.com>
2024-01-05 00:24:07 +0900

    feat: 헤더 추가
    헤더 추가
    
    #35

---
+++ FE/src/components/fundingDetail/InfoHeader.tsx
@@ -0,0 +1,13 @@
+export default function InfoHeader() {
+  return (
+    <>
+      <h1 className="text-[2.5rem] font-black text-color121">
+        나만의 귀여운 비즈 만들기 KIT
+      </h1>
+      <p className=" text-color9f9">
+        비즈 사랑 동아리 비사동에서 나만의 귀여운 비즈 반지 KIT를 펀딩하려고
+        합니다. 똑같아 보이지만 ‘내가 만들었다’는 가치를 담아보세요!
+      </p>
+    </>
+  );
+}
