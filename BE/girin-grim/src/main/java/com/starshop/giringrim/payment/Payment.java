b395582d4e6bd0f18c47b89e6803608de4ac0c65
minju <mkwak1125@gmail.com>
2024-01-07 16:09:08 +0900

    feat : payment 엔티티 작성

---
+++ BE/girin-grim/src/main/java/com/starshop/giringrim/payment/Payment.java
@@ -0,0 +1,33 @@
+package com.starshop.giringrim.payment;
+
+import com.starshop.giringrim.funding.entity.Funding;
+import com.starshop.giringrim.member.entity.Member;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name="payment_tb")
+public class Payment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "funding_id", nullable = false)
+    private Funding funding;
+    @Builder
+    public Payment(Member member, Funding funding){
+        this.member = member;
+        this.funding = funding;
+    }
+}
