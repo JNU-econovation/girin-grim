69307119ff19ffab11a1ef76eef63d1d84c69613
박건규 <geongyu09@gmail.com>
2024-01-08 13:26:13 +0900

    feat: 서버 요청 api 추가
    서버 요청 로직 추가
    
    #44

--- FE/src/components/fundingDetail/Detail/Description.tsx
+++ FE/src/components/fundingDetail/Detail/Description.tsx
@@ -1,16 +1,13 @@
+import useFundingDescription from "@/hooks/useFundingDescription";
 import Image from "next/image";
 
 export default function Description() {
+  const { data, isLoading, error } = useFundingDescription({ fundingId: 1 });
+  if (isLoading || !data) return <div>loading...</div>;
+  const imageUrl = data?.response.longDescription;
   return (
     <section className="max-w-xl mx-auto">
-      <Image
-        src={
-          "https://girin-grim.s3.ap-northeast-2.amazonaws.com/DetailImage.jpg"
-        }
-        alt="DetailImage"
-        width={1280}
-        height={720}
-      ></Image>
+      <Image src={imageUrl} alt="DetailImage" width={1280} height={720} />
     </section>
   );
 }
--- FE/src/components/fundingDetail/Detail/DetailDescription.tsx
+++ FE/src/components/fundingDetail/Detail/DetailDescription.tsx
@@ -9,6 +9,7 @@ export type NavList = "상세 설명" | "공지사항" | "문의" | "후기";
 
 export default function DetailDescription() {
   const [nav, setNav] = useState<NavList>(navList[0]);
+
   return (
     <section>
       <NavBar nav={nav} setNav={(nav: NavList) => setNav(nav)} />
