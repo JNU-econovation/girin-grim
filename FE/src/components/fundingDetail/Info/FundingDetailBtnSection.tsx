46e6258f47c2c8d43649a98c1dc69b176f869d92
박건규 <geongyu09@gmail.com>
2024-01-10 10:17:52 +0900

    feat: 만약 선택된 옵션이 없으면 결제 상세페이지로 넘어가지 않도록 기능 추가
    옵션이 없으면 결제 상세페이지로 넘어가지 않는다.
    
    #50

--- FE/src/components/fundingDetail/Info/FundingDetailBtnSection.tsx
+++ FE/src/components/fundingDetail/Info/FundingDetailBtnSection.tsx
@@ -2,6 +2,8 @@
 import { useRouter } from "next/navigation";
 import StyledBtn from "../../common/StyledBtn";
 import { Share } from "../../common/icon";
+import { useRecoilValue } from "recoil";
+import { SelectedOptions } from "@/store/FundingState";
 
 export default function FundingDetailBtnSection({
   fundingId,
@@ -9,6 +11,8 @@ export default function FundingDetailBtnSection({
   fundingId: number;
 }) {
   const router = useRouter();
+  const option = useRecoilValue(SelectedOptions);
+  const isOkToPledge = option.length > 0;
   return (
     <section className="flex gap-[0.63rem]">
       <button className="w-[4.375rem] h-[4.375rem] shrink-0 rounded-[0.25rem] flex justify-center items-center border mt-4 ">
@@ -16,7 +20,13 @@ export default function FundingDetailBtnSection({
       </button>
       <StyledBtn
         text="후원하기"
-        handler={() => router.push("/funding/pledge/" + fundingId)}
+        handler={() => {
+          if (!isOkToPledge) {
+            alert("옵션을 선택해주세요");
+            return;
+          }
+          router.push("/funding/pledge/" + fundingId);
+        }}
       />
     </section>
   );
