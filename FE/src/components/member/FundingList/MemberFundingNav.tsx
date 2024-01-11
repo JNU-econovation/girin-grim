35525d90da01402690ac0be3e0ea8a027297a34a
박건규 <geongyu09@gmail.com>
2024-01-11 18:58:56 +0900

    feat: 후원한 펀딩 네비게이션바 추가
    후원한 펀딩 네비게이션바 추가
    
    #59

--- FE/src/components/member/FundingList/MemberFundingNav.tsx
+++ FE/src/components/member/FundingList/MemberFundingNav.tsx
@@ -1,3 +1,19 @@
-export default function MemberFundingNav() {
-  return <div>nav bar</div>;
+import NavBar from "@/components/common/NavBar";
+
+type Props = {
+  navItems: string[];
+  setFocus: (nav: string) => void;
+  focused: string;
+};
+
+export default function MemberFundingNav({
+  navItems,
+  setFocus,
+  focused,
+}: Props) {
+  return (
+    <div className="w-64">
+      <NavBar navItems={navItems} setFocus={setFocus} focused={focused} />
+    </div>
+  );
 }
