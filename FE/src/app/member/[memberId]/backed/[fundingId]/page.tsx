7cb73174d7eff7f63620a4a4716999a2dc0ef1d5
박건규 <geongyu09@gmail.com>
2024-01-11 23:48:00 +0900

    feat: 후원 목록으로 가기 버튼 추가
    후원 목록으로 가기 버튼 추가
    
    #63

--- FE/src/app/member/[memberId]/backed/[fundingId]/page.tsx
+++ FE/src/app/member/[memberId]/backed/[fundingId]/page.tsx
@@ -1,11 +1,4 @@
-"use client";
-import FundingInfo from "@/components/backed/FundingInfo";
-import PageTitle from "@/components/common/PageTitle";
-import OptionContent from "@/components/common/OptionContent";
-import useBacked from "@/hooks/useBacked";
-import { formatOptionData } from "@/utils/dataFomat";
-import { Coin } from "@/components/common/icon";
-import Total from "@/components/common/Total";
+import BackedSection from "@/components/backed/BackedSection";
 
 type Props = {
   params: {
@@ -15,24 +8,9 @@ type Props = {
 };
 
 export default function BackedPage({ params: { memberId, fundingId } }: Props) {
-  const { data, error, isLoading } = useBacked({ fundingId, memberId });
-  if (isLoading || !data) return <div>로딩중</div>;
-  const { funding, member, options, address, totalPrice } = data.response;
-  const formatttedOptions = formatOptionData(options);
-  const supporter = {
-    address,
-  };
   return (
     <main className="w-full max-w-7xl mx-auto">
-      <PageTitle title="후원한 프로젝트" />
-      <section className="px-2">
-        <FundingInfo funding={funding} member={member} />
-        <OptionContent options={formatttedOptions} supporter={supporter} />
-        <section className="mt-[2.4rem]">
-          <h2 className="text-[1.375rem] font-extrabold">결제 정보</h2>
-          <Total totalCost={totalPrice} />
-        </section>
-      </section>
+      <BackedSection fundingId={fundingId} memberId={memberId} />
     </main>
   );
 }
---
+++ FE/src/components/backed/BackBtn.tsx
@@ -0,0 +1,19 @@
+import { useRouter } from "next/navigation";
+import StyledBtn from "../common/StyledBtn";
+
+type Props = {
+  memberId: number;
+};
+
+export default function BackBtn({ memberId }: Props) {
+  const redirectUrl = `/member/${memberId}`;
+  const router = useRouter();
+  const goBack = () => {
+    router.push(redirectUrl);
+  };
+  return (
+    <div className="w-[22.5rem] mx-auto mt-16">
+      <StyledBtn text="후원 목록으로" handler={() => goBack()} />
+    </div>
+  );
+}
