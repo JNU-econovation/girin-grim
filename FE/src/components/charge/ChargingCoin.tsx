28244b0574f454e9cf84e8d76a5f963318541a31
박건규 <geongyu09@gmail.com>
2024-01-12 16:44:06 +0900

    feat: 충전 금액 버튼 기능 추가
    충전 금액 버튼 기능 추가
    
    #66

--- FE/src/components/charge/ChargingCoin.tsx
+++ FE/src/components/charge/ChargingCoin.tsx
@@ -2,11 +2,11 @@
 import { paymentUnits } from "@/constants/ChargeDate";
 import PaymentAmountDisplay from "./PaymentAmountDisplay";
 import BalanceDisplay from "./BalanceDisplay";
-import { useRecoilValue } from "recoil";
+import { useRecoilState, useRecoilValue } from "recoil";
 import { TotalCoin } from "@/store/ChargeState";
 
 export default function ChargingCoin() {
-  const total = useRecoilValue(TotalCoin);
+  const [total, setTotal] = useRecoilState(TotalCoin);
   return (
     <>
       <div className="flex gap-1 items-end">
@@ -18,12 +18,13 @@ export default function ChargingCoin() {
       <p className="text-black text-6xl font-extrabold mt-5">{total}</p>
       <div className="bg-main mt-2 pt-1" />
       {paymentUnits.map((unit) => (
-        <div
+        <button
           className="inline-block bg-[#f2f2f2] mt-[0.63rem] mr-3 px-6 py-2 rounded-[0.25rem] cursor-default"
           key={unit}
+          onClick={() => setTotal(total + +unit)}
         >
           + {unit}
-        </div>
+        </button>
       ))}
       <PaymentAmountDisplay />
       <BalanceDisplay total={total} />
