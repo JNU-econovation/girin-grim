3f3cf678871703a0326855a2a9ffb1cb2501e945
minju <mkwak1125@gmail.com>
2024-01-12 19:58:23 +0900

    feat : test configuration 작성

---
+++ BE/girin-grim/src/test/java/com/starshop/giringrim/TestConfig.java
@@ -0,0 +1,25 @@
+package com.starshop.giringrim;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlConfig;
+
+
+@ActiveProfiles("test")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+public abstract class TestConfig {
+    @Autowired
+    private DatabaseCleanup databaseCleanup;
+
+    @AfterEach
+    public void setUpAfter() {
+        databaseCleanup.execute();
+    }
+
+}
