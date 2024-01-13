45444f21e63423064141f2745dbbcb41ddcc6641
박건규 <geongyu09@gmail.com>
2024-01-13 17:15:32 +0900

    design: 최종 금액 폰트 크기 수정
    최종 금액 폰트 크기 수정
    
    #71

--- FE/src/components/pages/fundingDetail/Info/FundingInfoCost.tsx
+++ FE/src/components/pages/fundingDetail/Info/FundingInfoCost.tsx
@@ -17,7 +17,7 @@ export default function FundingInfoCost({ coin, type }: Readonly<Props>) {
   return (
     <section className="w-full text-end">
       <p className="text-color999 font-semibold">My Coin : {coin} ( 충전 )</p>
-      <p className="text-xl font-extrabold flex justify-end gap-2 items-center">
+      <p className="text-4xl font-extrabold flex justify-end gap-2 items-center">
         <span className="opacity-70">{cost}</span>
         <Coin size="md" />
       </p>
