8cdb2ad4b80c1ce309e418ad4a635014c88937f1
박건규 <geongyu09@gmail.com>
2024-01-13 17:53:50 +0900

    design: 전문 gap 수정
    전문 gap 수정
    
    #71

--- FE/src/components/pages/pledge/Agreement.tsx
+++ FE/src/components/pages/pledge/Agreement.tsx
@@ -6,13 +6,13 @@ export default function Agreement() {
       <label htmlFor="1" className="flex justify-between mt-3 font-nanum">
         <input type="checkbox" className="mr-2" id="1" />
         <span className="w-full">[필수] 유의사항 확인 및 구매에 동의</span>
-        <span className="shrink-0 text-color999">(전문 열기)</span>
+        <span className="shrink-0 text-color999 mr-2">(전문 열기)</span>
         <ArrowDown />
       </label>
       <label htmlFor="2" className="flex justify-between my-1">
         <input type="checkbox" className="mr-2" id="2" />
         <span className="w-full">[필수] 정보 제3자 제공 확인 및 동의</span>
-        <span className="shrink-0 text-color999">(전문 열기)</span>
+        <span className="shrink-0 text-color999 mr-2">(전문 열기)</span>
         <ArrowDown />
       </label>
     </>
