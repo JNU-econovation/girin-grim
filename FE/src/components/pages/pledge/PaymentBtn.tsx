50a0048667ace3efe12141b43c04d5214e47e064
박건규 <geongyu09@gmail.com>
2024-01-15 11:34:14 +0900

    feat: 결제시 body에 입력한 주소 담기도록 수정
    결제시 body에 입력한 주소 담기도록 수정
    
    #88

--- FE/src/components/pages/pledge/PaymentBtn.tsx
+++ FE/src/components/pages/pledge/PaymentBtn.tsx
@@ -4,6 +4,8 @@ import StyledBtn from "../../common/StyledBtn";
 import { calTotalCost, getDonateCost } from "@/utils/payment";
 import { useState } from "react";
 import { postPayment } from "@/apis/funding";
+import { useRecoilValue } from "recoil";
+import { addressState } from "@/store/PledgeState";
 
 type Props = {
   fundingId: number;
@@ -19,6 +21,7 @@ export default function PaymentBtn({
   const [isLoading, setIsLoading] = useState(false);
   const { memberId, options } = getLocalData();
   let text = isLoading ? "결제중..." : "결제하기";
+  address = address ? address : useRecoilValue(addressState);
 
   const onClick = async () => {
     const total = type == "DONATE" ? getDonateCost() : calTotalCost(options);
@@ -27,13 +30,14 @@ export default function PaymentBtn({
       memberId,
       type,
       options,
-      address,
+      address: address ? address : "",
       price: total,
     });
     setIsLoading(false);
     if (returnedData.error) text = "결제 실패";
     else alert("결제가 완료되었습니다.");
   };
+
   return (
     <div className="mt-6">
       <StyledBtn text={text} handler={() => onClick()} />
