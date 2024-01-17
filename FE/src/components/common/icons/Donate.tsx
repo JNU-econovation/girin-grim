80f2b8c0b82178fef5039ab0da588e43430cb1c9
박건규 <geongyu09@gmail.com>
2024-01-18 00:12:32 +0900

    feat: 카테고리 변경시 강조 색상 적용
    카테고리 변경시 강조 색상 적용
    
    #105

--- FE/src/components/common/icons/Donate.tsx
+++ FE/src/components/common/icons/Donate.tsx
@@ -1,12 +1,13 @@
+"use client";
+import { HomeState } from "@/store/HomeState";
 import Image from "next/image";
+import { useRecoilValue } from "recoil";
 
 export default function Donate() {
+  const { category } = useRecoilValue(HomeState);
+  const selected = category === "기부형";
+  const src = selected ? "/assets/DonateSelected.svg" : "/assets/Donate.svg";
   return (
-    <Image
-      src="/assets/Donate.svg"
-      alt="category Donate select"
-      width={27}
-      height={25}
-    />
+    <Image src={src} alt="category Donate select" width={27} height={25} />
   );
 }
--- FE/src/components/common/icons/Gift.tsx
+++ FE/src/components/common/icons/Gift.tsx
@@ -1,9 +1,15 @@
+"use client";
+import { HomeState } from "@/store/HomeState";
 import Image from "next/image";
+import { useRecoilValue } from "recoil";
 
 export default function Gift() {
+  const { category } = useRecoilValue(HomeState);
+  const selected = category === "수령형";
+  const src = selected ? "/assets/GiftSelected.svg" : "/assets/Gift.svg";
   return (
     <Image
-      src="/assets/Gift.svg"
+      src={src}
       alt="category Gift select"
       width={27}
       height={20}
