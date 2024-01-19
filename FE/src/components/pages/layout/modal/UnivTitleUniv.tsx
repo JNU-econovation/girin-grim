dbaa9e1b84a7650c3a25e2da447b96437dca017e
박건규 <geongyu09@gmail.com>
2024-01-19 14:42:23 +0900

    feat: 디바운스 적용
    디바운스 적용
    
    #121

--- FE/src/components/pages/layout/modal/UnivTitleUniv.tsx
+++ FE/src/components/pages/layout/modal/UnivTitleUniv.tsx
@@ -1,17 +1,26 @@
 import { useRecoilState } from "recoil";
 import UnivTitle from "../../../common/UnivTitle";
 import { UnivStateQ, UnivStateRegion } from "@/store/HeaderState";
+import { useEffect, useState } from "react";
+import useDebounce from "@/hooks/useDebounce";
 
 export default function UnivTitleUniv() {
-  const [q, setQ] = useRecoilState(UnivStateQ);
+  const [_, setQ] = useRecoilState(UnivStateQ);
   const [region, setRegion] = useRecoilState(UnivStateRegion);
+  const [text, setText] = useState("");
+  const debounce = useDebounce(text, 500);
+
+  useEffect(() => {
+    setQ(debounce);
+  }, [debounce]);
+
   return (
     <UnivTitle
       title="대학"
-      data={q}
+      data={text}
       modify={(data: string) => {
         if (region != "") setRegion("전체");
-        setQ(data);
+        setText(data);
       }}
     />
   );
