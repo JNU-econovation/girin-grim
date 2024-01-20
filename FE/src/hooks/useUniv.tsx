8c88243fdfdc91495c0f99b2cc026ad985f4476d
박건규 <geongyu09@gmail.com>
2024-01-20 16:13:17 +0900

    feat: debounce 적용 위치 변경
    디바운스 젹용 위치 변경
    
    #135

--- FE/src/hooks/useUniv.tsx
+++ FE/src/hooks/useUniv.tsx
@@ -5,13 +5,16 @@ import { getUnivList } from "@/apis/uni";
 import { UnivStateQ, UnivStateRegion } from "@/store/HeaderState";
 import { useQuery } from "@tanstack/react-query";
 import { useRecoilValue } from "recoil";
+import useDebounce from "./useDebounce";
 
 export default function useUniv() {
   const region = useRecoilValue(UnivStateRegion);
   const q = useRecoilValue(UnivStateQ);
+  const keyword = useDebounce(q, 500);
+
   const univ = region == "전체" ? { region, q } : { region };
   const { data, isLoading, error } = useQuery<TResponse<Univs>>({
-    queryKey: ["univ", region || q],
+    queryKey: ["univ", region, keyword],
     queryFn: async () => await getUnivList(univ),
     staleTime: 1000 * 60 * 60 * 24,
   });
