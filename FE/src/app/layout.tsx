686635c407b6c97f07a36d49f2132c6e2be6b41c
박건규 <geongyu09@gmail.com>
2024-01-23 18:42:53 +0900

    feat: footer 위치 변경
    footer 위치 변경
    
    #147

--- FE/src/app/layout.tsx
+++ FE/src/app/layout.tsx
@@ -1,12 +1,8 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/pages/layout/Header";
 import { ReactQuery } from "@/context/ReackQuery";
 import Recoil from "@/context/Recoil";
-import { Footer } from "@/components/common/icon";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -27,7 +23,6 @@ export default function RootLayout({
             {children}
           </Recoil>
         </ReactQuery>
-        <Footer />
       </body>
     </html>
   );
