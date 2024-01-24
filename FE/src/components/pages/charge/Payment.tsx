2975642f06ff37ec8bdeceb645617d81347aacef
박건규 <geongyu09@gmail.com>
2024-01-24 18:15:47 +0900

    feat: 부족 충전 시 뒤로가지도록 동작
    부족 충신 시 뒤로 가지도록 동작 추가

--- FE/src/components/pages/charge/Payment.tsx
+++ FE/src/components/pages/charge/Payment.tsx
@@ -2,11 +2,11 @@
 import Total from "../../common/Total";
 import ChargeAgreement from "./ChargeAgreement";
 import ChargeBtn from "./ChargeBtn";
-import { useRecoilValue } from "recoil";
+import { useRecoilState } from "recoil";
 import { TotalCoin } from "@/store/ChargeState";
 
 export default function Payment() {
-  const totalCost = useRecoilValue(TotalCoin);
+  const [totalCost, setTotalCost] = useRecoilState(TotalCoin);
   return (
     <div className="relative py-11 px-10 z-50">
       <p className="text-[1.375rem] font-semibold">결제 정보를 확인해주세요.</p>
@@ -15,7 +15,7 @@ export default function Payment() {
       </div>
       <hr className="border-color999 border-2 mt-5" />
       <ChargeAgreement />
-      <ChargeBtn totalCost={totalCost} />
+      <ChargeBtn totalCost={totalCost} reset={() => setTotalCost(0)} />
     </div>
   );
 }
